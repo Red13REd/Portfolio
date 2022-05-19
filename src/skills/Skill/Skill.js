cabeb2a9b1d2bffe0c5396bb10fb7df67583937a
Red <amekysw@gmail.com>
2022-05-19 23:32:32 +0300

    2 lesson 2.1

--- src/skills/Skill/Skill.js
+++ src/skills/Skill/Skill.js
@@ -3,10 +3,10 @@ import style from './Skill.module.scss'
 
 export const Skill = (props) => {
     return (
-        <div className={style.skill}>
-            <div className={style.icon}></div>
-            <h3>{props.title}</h3>
-            <span>{props.description}</span>
-        </div>
+            <div className={style.skill}>
+                <div className={style.icon}></div>
+                <h3>{props.title}</h3>
+                <span>{props.description}</span>
+            </div>
     );
 };
