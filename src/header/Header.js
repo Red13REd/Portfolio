f648a980c21515cfee19624d0d3263c1dca2a88d
Red <amekysw@gmail.com>
2022-05-22 18:16:34 +0300

    2 lesson 2.4

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,29 @@
-import React from 'react';
 import style from './Header.module.scss'
 import {Nav} from "../nav/Nav";
+import {useEffect} from "react";
 
 export const Header = () => {
+
+    /*const handleScroll = () => {
+        console.log('Scrolling...');
+    };
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, true);
+
+        // Remove the event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll, true);
+        };
+    }, []);*/
+
     return (
-        <div className={style.header}>
-            <Nav/>
+        <div id="header" className={style.header}>
+            <div className={style.container}>
+                {/*<div className={style.logo}>LOGO</div>*/}
+                <Nav/>
+            </div>
         </div>
     );
-};
+}
